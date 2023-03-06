7817d07f2728d4141a45272601cd3007173091a4
Robin <64943572+Robspin@users.noreply.github.com>
2023-03-06 15:36:25 +0100

    pipeline hotfixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // For static sites images unoptimized
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
